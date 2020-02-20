b94eb7293723e2b25792ded54274f468f6fd16c7
sthtnr <sthtnr@gmail.com>
2020-02-20 12:33:17 +0900

    separate sql file into creation and population

---
+++ docker/postgres/sql/99_populateData.sql
@@ -0,0 +1,14 @@
+INSERT INTO todo_table
+  (content,deadline,done)
+VALUES
+  ('歯磨きをする', '12:00:00', true);
+
+INSERT INTO todo_table
+  (content,deadline,done)
+VALUES
+  ('散歩をする', '13:00:00', false);
+
+INSERT INTO todo_table
+  (content,deadline,done)
+VALUES
+  ('勉強をする', '14:00:00', true);
